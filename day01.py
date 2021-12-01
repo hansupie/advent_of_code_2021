bec62c0cf731cad4e1f39da2f22964e6cd5ee789
Hannemari Kuusisto <hannemari.kuusisto@tuni.fi>
2021-12-01 16:56:36 +0200

    day 1 part 2

--- day01.py
+++ day01.py
@@ -3,14 +3,21 @@
 with open("input.txt") as file:
     depth_list = file.readlines()
 
-prev_depth = 0
 depth_increased_count = 0
+prev_sum = 0
 
 for i, line in enumerate(depth_list):
-    depth = int(line)
-    # Check if depth measurement increased from the previous measurement
-    if i != 0 and depth > prev_depth:
+    if i >= (len(depth_list) - 3):
+        break
+    sum = 0
+    for line in depth_list[i:i+3]:
+        depth = int(line)
+        sum = sum + depth
+
+    # Check if sum depth measurements increased from the previous sum of measurements
+    if sum > prev_sum:
         depth_increased_count += 1
-    prev_depth = depth
+
+    prev_sum = sum
 
 print(depth_increased_count)
